7926f6e37be0d4fa1659534f9758cd1afaa6d700
tcmak217 <tcmak217@outlook.com>
2023-02-22 00:20:23 +0800

    feat: add basic html code and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import womanImage from "./images/illustration-woman-online-mobile.svg"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <img src={womanImage} alt="woman image" />
+      <div>FAQ</div>
+      <div>How many team members can I invite?</div>
+      <button></button>
+      <div>---</div>
+      <div>What is the maximum file upload size?</div>
+      <button></button>
+      <div>---</div>
+      <div>No more than 2GB. All files in your account must fit your allotted storage space.</div>
+      <div>How do I reset my password?</div>
+      <button></button>
+      <div>---</div>
+      <div>Can I cancel my subscription?</div>
+      <button></button>
+      <div>---</div>
+      <div>Do you provide additional support?</div>
+      <button></button>
+      <div>---</div>
+    </>
+  )
 }
 
 export default App
