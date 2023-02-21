dc2d28c33d9e364d49cace0814c9607bc530a9a6
tcmak217 <tcmak217@outlook.com>
2023-02-22 03:25:31 +0800

    feat: add background pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import womanImage from "./images/illustration-woman-online-mobile.svg"
+import backgroudPattern from "./images/bg-pattern-mobile.svg"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-bg-violet to-bg-blue">
         <div className="w-4/5 flex flex-col items-center bg-white rounded-3xl p-8 shadow-2xl relative">
           <img className="absolute -top-[5.8rem] w-52" src={womanImage} alt="woman image" />
+          <img className="absolute -top-1 w-52" src={backgroudPattern} alt="background pattern" />
           <div>FAQ</div>
           <div>How many team members can I invite?</div>
           <button></button>
