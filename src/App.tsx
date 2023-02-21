ff72999c1761ce3d0d349682d463d22673163227
tcmak217 <tcmak217@outlook.com>
2023-02-22 00:34:41 +0800

    feat: add basic layout using flex box

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,30 @@ import womanImage from "./images/illustration-woman-online-mobile.svg"
 function App() {
   return (
     <>
-      <img src={womanImage} alt="woman image" />
-      <div>FAQ</div>
-      <div>How many team members can I invite?</div>
-      <button></button>
-      <div>---</div>
-      <div>What is the maximum file upload size?</div>
-      <button></button>
-      <div>---</div>
-      <div>No more than 2GB. All files in your account must fit your allotted storage space.</div>
-      <div>How do I reset my password?</div>
-      <button></button>
-      <div>---</div>
-      <div>Can I cancel my subscription?</div>
-      <button></button>
-      <div>---</div>
-      <div>Do you provide additional support?</div>
-      <button></button>
-      <div>---</div>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="w-4/5 flex flex-col items-center">
+          <img src={womanImage} alt="woman image" />
+          <div>FAQ</div>
+          <div>How many team members can I invite?</div>
+          <button></button>
+          <div>---</div>
+          <div>What is the maximum file upload size?</div>
+          <button></button>
+          <div>---</div>
+          <div>
+            No more than 2GB. All files in your account must fit your allotted storage space.
+          </div>
+          <div>How do I reset my password?</div>
+          <button></button>
+          <div>---</div>
+          <div>Can I cancel my subscription?</div>
+          <button></button>
+          <div>---</div>
+          <div>Do you provide additional support?</div>
+          <button></button>
+          <div>---</div>
+        </div>
+      </div>
     </>
   )
 }
