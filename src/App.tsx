c94de6e2bd8433f0d84734bbeeb07f0d51ba424b
tcmak217 <tcmak217@outlook.com>
2023-02-22 11:56:18 +0800

    feat: add better layout to the content

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,29 @@ function App() {
         <div className="w-4/5 flex flex-col items-center bg-white rounded-3xl p-8 shadow-2xl relative">
           <img className="absolute -top-[5.8rem] w-52" src={womanImage} alt="woman image" />
           <img className="absolute -top-1 w-52" src={backgroudPattern} alt="background pattern" />
-          <div>FAQ</div>
-          <div>How many team members can I invite?</div>
-          <button></button>
-          <div>---</div>
-          <div>What is the maximum file upload size?</div>
-          <button></button>
-          <div>---</div>
-          <div>
-            No more than 2GB. All files in your account must fit your allotted storage space.
+          <div className="flex flex-col items-center mt-20">
+            <div className="text-4xl font-bold">FAQ</div>
+            <div className="flex flex-col items-start mt-8">
+              <div>How many team members can I invite?</div>
+              <button></button>
+              <div>---</div>
+              <div>What is the maximum file upload size?</div>
+              <button></button>
+              <div>---</div>
+              <div>
+                No more than 2GB. All files in your account must fit your allotted storage space.
+              </div>
+              <div>How do I reset my password?</div>
+              <button></button>
+              <div>---</div>
+              <div>Can I cancel my subscription?</div>
+              <button></button>
+              <div>---</div>
+              <div>Do you provide additional support?</div>
+              <button></button>
+              <div>---</div>
+            </div>
           </div>
-          <div>How do I reset my password?</div>
-          <button></button>
-          <div>---</div>
-          <div>Can I cancel my subscription?</div>
-          <button></button>
-          <div>---</div>
-          <div>Do you provide additional support?</div>
-          <button></button>
-          <div>---</div>
         </div>
       </div>
     </>
