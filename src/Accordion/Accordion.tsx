18bf7714e8f10a3cbadc70912a5ac42da2c66b74
tcmak217 <tcmak217@outlook.com>
2023-02-22 16:43:56 +0800

    fix: fix the image width and position

--- src/Accordion/Accordion.tsx
+++ src/Accordion/Accordion.tsx
@@ -7,9 +7,9 @@ function Accordion({ accordionData }: { accordionData: { question: string; answe
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-bg-violet to-bg-blue">
       <div className="w-5/6 flex flex-col items-center bg-white rounded-3xl p-8 shadow-2xl relative">
-        <img className="absolute -top-[5.8rem] w-52" src={womanImage} alt="woman image" />
-        <img className="absolute -top-1 w-52" src={backgroudPattern} alt="background pattern" />
-        <div className="flex flex-col items-center mt-20">
+        <img className="absolute -top-[9rem] w-80" src={womanImage} alt="woman image" />
+        <img className="absolute -top-1 w-80" src={backgroudPattern} alt="background pattern" />
+        <div className="flex flex-col items-center mt-36">
           <div className="text-4xl font-bold">FAQ</div>
           <div className="flex flex-col items-start mt-8">
             {accordionData.map((item, index) => (
