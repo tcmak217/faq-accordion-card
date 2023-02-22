69544fa4a2eee932c7b2c3e447f51b82d7b876b3
tcmak217 <tcmak217@outlook.com>
2023-02-22 15:51:43 +0800

    feat: add toggle to the accordion items

--- src/AccordionItem/AccordionItem.tsx
+++ src/AccordionItem/AccordionItem.tsx
@@ -1,13 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import arrowDown from "../images/icon-arrow-down.svg"
 
 function AccordionItem({ question, answer }: { question: string; answer: string }) {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleToggle = () => {
+    setIsActive(!isActive)
+  }
+
   return (
-    <div className="border-b-2 w-full flex flex-row justify-between">
-      <div className="w-3/4">{question}</div>
-      <button>
-        <img src={arrowDown} alt="arrow down" />
-      </button>
+    <div className="border-b-2 w-full flex flex-col justify-center">
+      <div className="flex flex-row" onClick={() => handleToggle()}>
+        <div className="w-4/5">{question}</div>
+        <div className="w-1/5 flex flex-row justify-end items-center">
+          <img
+            className={`w-3 ${isActive ? "" : "scale-y-[-1]"}`}
+            src={arrowDown}
+            alt="arrow down"
+          />
+        </div>
+      </div>
+      {isActive && <div className="w-full">{answer}</div>}
     </div>
   )
 }
