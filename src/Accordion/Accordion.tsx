219018a4beffcdde01cec2a43f3f9c46cb5146ba
tcmak217 <tcmak217@outlook.com>
2023-02-22 16:49:31 +0800

    feat: add more padding in bottom of the accordion and expand the flex box to full width

--- src/Accordion/Accordion.tsx
+++ src/Accordion/Accordion.tsx
@@ -6,12 +6,12 @@ import AccordionItem from "../AccordionItem/AccordionItem"
 function Accordion({ accordionData }: { accordionData: { question: string; answer: string }[] }) {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-bg-violet to-bg-blue">
-      <div className="w-5/6 flex flex-col items-center bg-white rounded-3xl p-8 shadow-2xl relative">
+      <div className="w-5/6 flex flex-col items-center bg-white rounded-3xl p-8 pb-16 shadow-2xl relative">
         <img className="absolute -top-[9rem] w-80" src={womanImage} alt="woman image" />
         <img className="absolute -top-1 w-80" src={backgroudPattern} alt="background pattern" />
-        <div className="flex flex-col items-center mt-36">
+        <div className="flex flex-col items-center mt-36 w-full">
           <div className="text-4xl font-bold">FAQ</div>
-          <div className="flex flex-col items-start mt-8">
+          <div className="flex flex-col items-start mt-8 w-full">
             {accordionData.map((item, index) => (
               <AccordionItem question={item.question} answer={item.answer}></AccordionItem>
             ))}
